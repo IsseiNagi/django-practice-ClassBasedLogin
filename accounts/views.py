ba9fad7af67429b13145614d7ab76b4b12c59438
Issei Nagi <issei.nagi@gmail.com>
2021-08-19 10:47:31 +0900

    モデル・テンプレート・基本View作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, FormView
+from django.views.generic.base import TemplateView, View
+from .forms import RegistForm
 
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+class RegistUserView(CreateView):
+    template_name = 'regist.html'
+    form_class = RegistForm
+
+
+# FormViewを継承してログインビューを作る
+class UserLoginView(FormView):
+    pass
+
+
+# Viewを継承してログアウトビューを作る
+class UserLogoutView(View):
+    pass
